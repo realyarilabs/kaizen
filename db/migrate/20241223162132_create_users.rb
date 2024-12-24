828c8bba9039e6e606ee09ce71ce1d915207236f
Rodrigues Susana <rodrigues_susana@outlook.com>
2024-12-24 14:55:32 +0000

    refactor user migration to add unique index for email after table creation

--- db/migrate/20241223162132_create_users.rb
+++ db/migrate/20241223162132_create_users.rb
@@ -3,11 +3,12 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false, unique: true, index: true
+      t.string :email, null: false
       t.integer :role, null: false, default: 1, index: true
       t.boolean :active, null: false, default: true, index: true
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
