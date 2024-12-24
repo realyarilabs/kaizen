243439c3655b2b1070f42dc75f86d1275ef34183
Rodrigues Susana <rodrigues_susana@outlook.com>
2024-12-24 14:46:07 +0000

    add join table migration for suggestions and tags

---
+++ db/migrate/20241224143955_create_join_table_suggestions_tags.rb
@@ -0,0 +1,9 @@
+class CreateJoinTableSuggestionsTags < ActiveRecord::Migration[8.0]
+  def change
+    create_join_table :suggestions, :tags do |t|
+      t.index :suggestion_id
+      t.index :tag_id
+      t.index [:suggestion_id, :tag_id], unique: true
+    end
+  end
+end
