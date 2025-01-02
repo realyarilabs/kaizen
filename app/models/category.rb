7d55d47466b7aff0d300656002e76dc35775bff9
Rodrigues Susana <rodrigues_susana@outlook.com>
2025-01-02 15:07:32 +0000

    Add Tailwind CSS and Flowbite integration

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@
 
 # Category model
 class Category < ApplicationRecord
-  has_and_belongs_to_many :suggestions, dependent: :restrict_with_error
+    has_and_belongs_to_many :suggestions, dependent: :restrict_with_error
 end
